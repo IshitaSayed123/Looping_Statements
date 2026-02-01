c4a46c92304a0da51f5a2ca54a9693d202e666de
Sayed Ishita Jaha <sayedishita123@gmail.com>
2026-02-01 19:29:15 +0530

    Add do-while loop to generate multiplication table

---
+++ dowhile_table5.java
@@ -0,0 +1,12 @@
+package looping_statements;
+public class do_while_table5 {
+	public static void main(String[] args) {
+		int i=1;int mul; int num=5;
+		do {
+			mul=i*num;
+			System.out.println(num+"*"+i+"="+mul);
+			i++;
+		}while(i<=10);	
+	}
+}
+

d1964fd85fed8a7c5cc8208e028e7a8c3d668297
Sayed Ishita Jaha <sayedishita123@gmail.com>
2026-02-01 19:28:23 +0530

    Add do-while loop example in do_while_task.java

---
+++ do_while_task.java
@@ -0,0 +1,10 @@
+package looping_statements;
+public class do_while_task {
+	public static void main(String[] args) {
+		int i=1;
+		do {
+			System.out.println("Loop is executed");
+			i++;
+		}while(i<=1);
+	}
+}
